e1d0eebb174ff69e83027a3d6264f5b51af0089d
Celysus <martyaldridge@gmail.com>
2015-12-31 14:10:36 -0800

    did the button thing

--- src/syncjam/ui/buttons/base/ButtonUI.java
+++ src/syncjam/ui/buttons/base/ButtonUI.java
@@ -4,6 +4,7 @@ import syncjam.SongUtilities;
 import syncjam.ui.Colors;
 
 import javax.swing.*;
+import javax.swing.plaf.metal.MetalButtonUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -32,6 +33,9 @@ public abstract class ButtonUI extends JButton implements ActionListener
         this.setBorderPainted(false);
         this.setFocusPainted(false);
         this.setFocusable(false);
+        this.setUI(new ButtonUIStyle());
+        this.setBackground(background);
+
         background = bg;
         songUtilities = utils;
     }
@@ -48,6 +52,7 @@ public abstract class ButtonUI extends JButton implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        setBackground(background);
         if(e.getActionCommand().equals("clicked"))
         {
             clicked();
@@ -56,14 +61,31 @@ public abstract class ButtonUI extends JButton implements ActionListener
 
     public void paintComponent(Graphics g)
     {
-        g.setColor(background);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        setBackground(background);
+        super.paintComponent(g);
+        //g.setColor(background);
+        //g.fillRect(0, 0, getWidth(), getHeight());
 
-        if (getModel().isPressed())
+        if (getModel().isPressed()) {
             g.setColor(Colors.c_Highlight);
-        else if (getModel().isRollover())
+            setForeground(Colors.c_Highlight);
+        }
+        else if (getModel().isRollover()) {
             g.setColor(Colors.c_Foreground1);
-        else
+            setForeground(Colors.c_Foreground1);
+        }
+        else {
             g.setColor(Colors.c_Foreground2);
+            setForeground(Colors.c_Foreground2);
+        }
+    }
+}
+class ButtonUIStyle extends MetalButtonUI
+{
+    @Override
+    public void paintButtonPressed(Graphics g, AbstractButton b) {
+        paintText(g, b, b.getBounds(), b.getText());
+        g.setColor(Colors.c_Background1);
+        g.fillRect(0, 0, b.getSize().width, b.getSize().height);
     }
 }
